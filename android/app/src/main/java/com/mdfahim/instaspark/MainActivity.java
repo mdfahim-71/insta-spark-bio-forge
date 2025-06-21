85df43ba4a6ff20d314c1d664b14706259fca80f
mdfahim-71 <fhsatisfied71@gmail.com>
2025-06-21 15:14:14 +0000

    feat: Add Capacitor and Android platform

---
+++ android/app/src/main/java/com/mdfahim/instaspark/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mdfahim.instaspark;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mdfahim.instaspark',
+  appName: 'InstaSpark Bio Forge',
+  webDir: 'dist'
+};
+
+export default config;
